29b3c02e4a0cff95a7bf86804a27110511132579
alondmart <m10917032@gmail.com>
2024-04-02 18:51:26 -0600

    just uploading initial favicon stuff

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+// adds current year in footer
+const currentYear = new Date()
+document.querySelector("#year").innerHTML = currentYear.getFullYear()
+
+function toggleMenu() {
+    document.querySelector("#navItems").classList.toggle("open");
+    document.querySelector("#menuBtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#menuBtn")
+
+x.onclick = toggleMenu;
